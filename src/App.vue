f8a6a557fc0daa2e4b60eff9d11f3288cce153db
GuilleFerres <guillermoferressanchez@gmail.com>
2024-08-11 20:33:08 +0200

    Feat(HomeView): 'Finish section 5, methods, computed and watch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
     <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -12,8 +7,30 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
+
 nav {
-  display: flex;
-  justify-content: center;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}:</h3>
+    <div>
+      <button class="btn" @click="decreaseCounter(2)">--</button>
+      <button class="btn" @click="decreaseCounter(1)">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button class="btn" @click="increaseCounter(1)">+</button>
+      <button class="btn" @click="increaseCounter(2)">++</button>
+    </div>
+    <p>This counter is {{ oddOrEven }}</p>
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
+    </div>
   </div>
 </template>
+<script setup>
+import { computed, reactive, ref, watch } from 'vue';
+// const counter = ref(0),
+     // counterTitle = ref("My Counter");
+const appTitle = "My amazing Counter app";
+const counterData = reactive({
+  count: 0,
+  title: "My Counter"
+});
+const count = ref();
+watch(() => counterData.count, (newCount) => {
+  if (newCount === 20) {
+    alert("Way to go!! You made it to 20!!")
+  }
+})
+const oddOrEven = computed(()=> {
+  if(counterData.count % 2 === 0) return "even"
+  return "odd";
+})
+
+// methods
+const increaseCounter = amount => {
+  counterData.count += amount;
+}
+const decreaseCounter = amount => {
+  counterData.count -= amount;
+}
+</script>
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit {
+  margin-top: 60px;
+}
+</style>
